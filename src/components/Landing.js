9d9766784b1a804abced96a58b42e2b02f77e9a0
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-01 13:03:40 +0400

    <div className="restaurant_select_bottom">

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,28 @@
 import React from "react";
-
-
 class Landing extends React.Component{
     render() {
         return(
-        <div className="restaurant_select">
-       <div className="restaurant_select_top">
-        <div className="restaurant_select_top-header">Выбери Ресторан</div>
-        <div className="arrow_picker"></div>
-        <div className="arrow_picker-up"></div>
-        <div className="arrow_picker-down"></div>
-        <div className="restaurant_select_bottom"></div>
-        </div>
-        </div>
+       <div className="restaurant_select">
+                <div className="restaurant_select_top">
+                    <div className="restaurant_select_top-header">Выбери Ресторан</div>
+
+                    <div className="arrow_picker">
+                    <div className="arrow_picker-up"></div>
+                    <div className="arrow_picker-down"></div>
+                </div>
+                </div>
+           
+           <div className="restaurant_select_bottom">
+           <ul>
+                    <li>rest 1</li>
+                    <li>rest 1</li>
+                    <li>rest 1</li>
+                </ul>
+            </div>
+            <button>Перейти в ресторан</button>
+            </div>
+
+        
         )
     }
 }
