3260d1eb57e18d1b685c56c03e883b24beeae620
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-01 15:02:43 +0400

    в app создала компоненты для отображения Header Order MenuAdmin

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,14 @@ import React from "react";
 
 class App extends React.Component{
     render(){
-        return
+        return (
+            <div className="burger-paradise">
+                <div className="menu">
+                    <Header/>
+                </div>
+                <Order/>
+                <MenuAdmin/>
+            </div>
+        )
     }
 }
