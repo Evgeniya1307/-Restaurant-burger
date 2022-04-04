7471da52a1165106ac0bd96be57830b2b06aa8ff
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-04 13:58:18 +0400

    <button type="sumbit">Добавить в меню</button>

--- src/components/AddBurgerForm.js
+++ src/components/AddBurgerForm.js
@@ -1,17 +1,23 @@
 import React from "react";
 
 class AddBurgerForm extends React.Component {
+  createBurger=(event)=>{
+console.log("add burger!!!!")
+}
   render() {
-    return <form className="burger-edit">
-      <input name="name" type={text} placeholder="Name" autoComplete="off"/>
-      <input name="price" type={text} placeholder="Price"autoComplete="off"/>
+    return(
+       <form className="burger-edit" onSubmit={this.createBurger}>
+      <input name="name" type="text" placeholder="Name" autoComplete="off"/>
+      <input name="price" type="ext" placeholder="Price"autoComplete="off"/>
       <select name="status" className="status">
-<option value={available}>Доступно</option>
-<option value={unavailable}>Убракть из Меню</option>
+<option value="available">Доступно</option>
+<option value="unavailable">Убракть из Меню</option>
       </select>
       <textarea name="desc"  placeholder="Desc"/>
-      <input name="image" type={text} placeholder="Image" autoComplete="off"/>
-    </form>;
+      <input name="image" type="text" placeholder="Image" autoComplete="off"/>
+      <button type="sumbit">Добавить в меню</button>
+    </form>
+    );
   }
 }
 
