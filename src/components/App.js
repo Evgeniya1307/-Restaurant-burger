7bf16ef62f6762bde7d6a3388387a0c3c074eeb5
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-04 22:40:06 +0400

    const image = this.props.details.image;         const name = this.props.details.name;

--- src/components/App.js
+++ src/components/App.js
@@ -4,40 +4,48 @@ import Order from "./Order";
 import MenuAdmin from "./MenuAdmin";
 import  sampleBurgers from "../sample-burgers";
 import Burger from "./Burger";
+
 class App extends React.Component {
  state={
    burgers: {},
    order:{}
- }
+ };
 
- addBurger=(burger)=>{
+ addBurger= burger=> {
  //1.чтобы добавить в state наш бургер:делаем копию обьекта  state создаё1м новую переменную const  и используем оператор спрет ...this.state.burgers,теперь в этот обьект должны добавить наш новый обьект бургер:burgers[`burger`${Date.now(этот метод даёт уникальную метку милисекундах используем для нумерации наших бургеров для экономии времени)}]
-  const burgers={...this.state.burgers}
+  const burgers = {...this.state.burgers};
 //2. добавить новый бургер в  переменную burgers
-  burgers[`burger${Date.now}`]=burger;
+  burgers[`burger${Date.now()}`]=burger;
 //3.используем метод setstate чтобы записать обновлёный обьект бургерс в наш обьект state
 this.setState({burgers});
 };
 
-loadSampleBurgers=()=> {
-this.setState({burgers:sampleBurgers});
+loadSampleBurgers = ()=> {
+console.log("Ready To Load!!!")
+  this.setState({burgers:sampleBurgers});
 };
 
   render() {
     return (
-      <div className="burger-paradise">
+    <div className="burger-paradise">
         <div className="menu">
-          <Header title="Hot Burger" />
+          <Header title="Hot Burgers" />
         <ul className="burgers">
           {Object.keys(this.state.burgers).map(key=>{
-            return<li>key</li>
+            return(
+            <Burger 
+            key={key}
+              index={key}
+              details={this.state.burgers[key]}
+            />
+            );
           })}
         </ul>
         </div>
         <Order />
         <MenuAdmin 
         addBurger={this.addBurger}
-          LoadSampleBurgers={this.loadSampleBurgers}
+        loadSampleBurgers={this.loadSampleBurgers}
         />
       </div>
     );
--- src/components/Burger.js
+++ src/components/Burger.js
@@ -4,8 +4,19 @@ import React from "react";
 
 class Burger extends React.Component {
     render(){
+        const image = this.props.details.image;
+        const name = this.props.details.name;
         return(
-            <p>Burger Component</p>
+            <li className="menu-burger">
+                <div className="image-container">
+                    <img src={image}/>
+                </div>
+                <div className="burger-details">
+                    <h3 className="burger-name">{name}
+
+                    </h3>
+                </div>
+            </li>
         )
     }
 }
