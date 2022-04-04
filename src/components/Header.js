a1af8927b86cace0a4df744480b3728780d62027
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-04 12:22:49 +0400

    сщздала router.js  применила маршрутизацию для страниц

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-const  Header=props=>{
-    return(
+const  Header=props=>(
         <header className="top">
                <div className="wrap">
                    <div className="header-content">
@@ -25,32 +24,5 @@ const  Header=props=>{
         
     
     )
-}
 
-// class Header extends React.Component{
-//     render(){
-//         return (
-//            <header className="top">
-//                <div className="wrap">
-//                    <div className="header-content">
-                      
-//                        <div className="header-rating">
-//                            <div className="header-rating_tag">Рейтинг:  </div>
-//                                <div className="header-rating_icon">★★★★★</div>
-//                        </div>
-// <div className="header-divider"></div>
-// <h1 className = "font-effect-fire-animation">{this.props.title}</h1>
-// <h3>
-//     <span>
-//         Быстрая доставка горячих
-//         <span className="sub-header"> #бургеров</span>
-//     </span>
-// </h3>
-
-//                    </div>
-//                </div>
-//            </header>
-//         )
-//     }
-// }
 export default Header;
