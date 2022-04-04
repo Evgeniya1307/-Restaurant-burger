27497f488ea3af46d7348e34db6503511eb83c7e
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-04 14:19:11 +0400

    создала обьект кот орый принимает все значения из нашей формы const burger={

--- src/components/AddBurgerForm.js
+++ src/components/AddBurgerForm.js
@@ -3,7 +3,7 @@ import React from "react";
 class AddBurgerForm extends React.Component {
   nameRef = React.createRef();
   priceRef = React.createRef();
-  stausRef = React.createRef();
+  statusRef = React.createRef();
   descRef = React.createRef();
   imageRef = React.createRef();
   
@@ -11,19 +11,22 @@ class AddBurgerForm extends React.Component {
   
   createBurger=(event)=>{
     event.preventDefault();
-console.log("add burger!!!!")
+console.log("add burger!!!!", this.nameRef.current.value)
+const burger={
+  name: this.nameRef.current.value,
 }
+};
   render() {
     return(
        <form className="burger-edit" onSubmit={this.createBurger}>
-      <input name="name" type="text" placeholder="Name" autoComplete="off"/>
-      <input name="price" type="ext" placeholder="Price"autoComplete="off"/>
-      <select name="status" className="status">
+      <input ref={this.nameRef} name="name" type="text" placeholder="Name" autoComplete="off"/>
+      <input ref={this.priceRef} name="price" type="ext" placeholder="Price"autoComplete="off"/>
+      <select ref={this.statusRef} name="status" className="status">
 <option value="available">Доступно</option>
 <option value="unavailable">Убракть из Меню</option>
       </select>
-      <textarea name="desc"  placeholder="Desc"/>
-      <input name="image" type="text" placeholder="Image" autoComplete="off"/>
+      <textarea ref={this.descRef} name="desc"  placeholder="Desc"/>
+      <input ref={this.imageRef} name="image" type="text" placeholder="Image" autoComplete="off"/>
       <button type="sumbit">Добавить в меню</button>
     </form>
     );
