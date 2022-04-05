a546cc5fa64477ae3e72596f3434bc7333f5c211
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-05 13:15:15 +0400

    создали  const orderIds = Object.keys(this.props.order) используем ментод object.keys  чтобы получить массив из ключей  ь=бургеров кот орые мы заказываем их айди находятся в this.props.order

--- src/components/Order.js
+++ src/components/Order.js
@@ -4,6 +4,7 @@ import React from "react";
 
 class Order extends React.Component{
     render(){
+        const orderIds = Object.keys(this.props.order)
         return (
             <div className="order-wrap">
              <h2>Ваш заказ</h2>   
