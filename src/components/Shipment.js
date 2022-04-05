56531f4eed15705c3b00e4b21204ad6cecd654d8
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-05 14:44:44 +0400

    создала переменную shipping(доставка) если сумма заказа выше 0 и в тоже самое время сум ма заказа м еньше 500 то стоимость доставки 350р в противном случае если сумма заказа больше 500 рублей то доставка 99 р     const shipping = total > 0 && total < 500 ? 350 : 99;

--- src/components/Shipment.js
+++ src/components/Shipment.js
@@ -4,6 +4,8 @@ import React from "react";
 class Shipment  extends React.Component{
 render(){
     const {total}=this.props;
+    //создала переменную shipping(доставка) если сумма заказа выше 0 и в тоже самое время сум ма заказа м еньше 500 то стоимость доставки 350р в противном случае если сумма заказа больше 500 рублей то доставка 99 р
+    const shipping = total > 0 && total < 500 ? 350 : 99;
     return(
         <div className="total">
             <div className="total_wrap">
