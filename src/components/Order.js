3c7d99a95b3edd198ab12c8e828567e31aa848ed
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-05 15:46:02 +0400

    сщздали base.js  там иморты базы данных с реактом

--- src/components/Order.js
+++ src/components/Order.js
@@ -6,7 +6,8 @@ class Order extends React.Component{
    renderOrder =(key)=>{
     const burger = this.props.burgers[key];
     const count = this.props.order[key];
-      const isAvailable=burger&&burger.status==="available";//создала переменную и проверку 
+      
+    const isAvailable=burger && burger.status==="available";//создала переменную и проверку 
     if (!isAvailable){
         return( 
         <li className="unavailable" key ={key}>
@@ -19,7 +20,7 @@ class Order extends React.Component{
         <span>
             <span>{count}</span>
        шт.{burger.name}
-       <span>{count*burger.price}₽</span>
+       <span>{count * burger.price}₽</span>
         <button className="cancellItem">&times;</button>
         </span>
     </li>
@@ -30,25 +31,22 @@ class Order extends React.Component{
    
     render(){
         const orderIds = Object.keys(this.props.order);
-        const total = orderIds.reduce((prevTotal,key)=>{
+        const total = orderIds.reduce((prevTotal,key) => {
             const burger = this.props.burgers[key];
             const count = this.props.order[key];
        
-            const  isAvailable =burger && burger.status === "available";
-            if (isAvailable){
-                return prevTotal + burger.price*count;
+            const  isAvailable = burger && burger.status === "available";
+            if (isAvailable) {
+                return prevTotal + burger.price * count;
             }
-            return prevTotal;//добавили проверку если бургер доступен то мы его учитываем в общей сумме заказа если нет то не учитываем возвращаем сумму без кокректного бургера которого нет
-       
-            
+            return prevTotal;//добавили проверку если бургер доступен то мы его учитываем в общей сумме заказа если нет то не учитываем возвращаем сумму без кокректного бургера которого нет    
         },0);
         
         return (
             <div className="order-wrap">
              <h2>Ваш заказ</h2>   
-             <ul className="order">
-             {orderIds.map(this.renderOrder)}</ul>
-            {total>0?Order(
+             <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+            {total > 0 ? (
                 <Shipment total = {total}/>
             ):(
                 <div className="nothingSelected">
