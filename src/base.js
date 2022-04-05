d8ea9101d0e3951fd57f7fae00c5c60c295e40bc
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-05 16:08:12 +0400

    const base = Rebase .createClass(firebaseApp.database());// создали базу данных

--- src/base.js
+++ src/base.js
@@ -2,5 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    
-})
+    apiKey: "AIzaSyB72tQzZIfl0TdX71_ihFUApTi3SY5HqLg",
+  authDomain: "very-hot-burgers-ae4b2.firebaseapp.com",
+  databaseURL: "https://very-hot-burgers-ae4b2-default-rtdb.firebaseio.com",
+});
+
+const base = Rebase .createClass(firebaseApp.database());// создали базу данных
